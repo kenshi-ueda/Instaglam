19740c7378d4b0cff2fc3572012151ff551a9ce9
kenshi.ueda <Kenshi150107@gmail.com>
2021-02-28 15:12:16 +0900

    Initial Commit

---
+++ instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by ueda on 2021/02/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
