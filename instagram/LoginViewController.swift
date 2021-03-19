282abdecbcee7c6a7ddd44180ef223e6ead66961
kenshi.ueda <Kenshi150107@gmail.com>
2021-03-19 16:45:20 +0900

    再提出

--- instagram/LoginViewController.swift
+++ instagram/LoginViewController.swift
@@ -29,6 +29,7 @@ class LoginViewController: UIViewController {
             Auth.auth().signIn(withEmail: address, password: password) { authResult, error in
                 if let error = error {
                     print("DEBUG_PRINT: " + error.localizedDescription)
+                    SVProgressHUD.showError(withStatus: "サインインに失敗しました。")
                     return
                 }
                 print("DEBUG_PRINT: ログインに成功しました。")
