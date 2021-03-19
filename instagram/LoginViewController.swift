2b4b489007dd8aa3d192428e515817108439efd2
kenshi.ueda <Kenshi150107@gmail.com>
2021-03-19 18:26:26 +0900

    再々

--- instagram/LoginViewController.swift
+++ instagram/LoginViewController.swift
@@ -49,6 +49,7 @@ class LoginViewController: UIViewController {
             // アドレスとパスワードと表示名のいずれかでも入力されていない時は何もしない
             if address.isEmpty || password.isEmpty || displayName.isEmpty {
                 print("DEBUG_PRINT: 何かが空文字です。")
+                SVProgressHUD.showError(withStatus: "必要項目を入力して下さい")
                 return
             }
             
@@ -60,6 +61,7 @@ class LoginViewController: UIViewController {
                 if let error = error {
                     // エラーがあったら原因をprintして、returnすることで以降の処理を実行せずに処理を終了する
                     print("DEBUG_PRINT: " + error.localizedDescription)
+                    SVProgressHUD.showError(withStatus: "ユーザー作成に失敗しました。")
                     return
                 }
                 print("DEBUG_PRINT: ユーザー作成に成功しました。")
